72887be898a13e35302b7a10975ae8fc89c43247
MartaSch <martaschab27@gmail.com>
2023-06-16 21:42:23 +0200

    hooks-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import ContactForm from './ContactsForm/ContactsForm';
+import ContactList from "./ContactList/ContactList"
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Filter from "./Filter/Filter";
+
+const initialContacts = [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+]
+
+export default function App() {
+  
+const [contacts, setContacts] = useState(initialContacts)
+const [filter, setFilter] = useState('')
+
+  const addContact = ({ id, name, number }) => {
+     const contact = {
+      id,
+      name,
+      number,
+     }
+     const nameContacts = contacts.find(
+      contact => contact.name === name
+     )
+     if(nameContacts) {
+      alert(`${name} is already in contacts`);
+     } else {
+      setContacts([contact, ...contacts])
+      }
+     }
+  
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id)
+    )};
+
+  const handleFilter = e => {
+   setFilter(e.currentTarget.value)
+  };
+
+  const getFilteredContacts = () => {
+    return contacts.filter(contact => 
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+
+    return (
+      <div>
+    <ContactForm
+    onSubmit={addContact}
+    />
+    <Filter
+      handleFilter={handleFilter}
+    />
+    <ContactList
+    key={nanoid()}
+      listContacts = {getFilteredContacts()}
+      onDeleteContact={deleteContact}
+    />
     </div>
-  );
-};
+    )
+  }
---
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import css from './ContactsForm.module.css';
+import { nanoid } from 'nanoid';
+
+const ContactForm = ({ onSubmit }) => {
+  const [state, setState] = useState({
+    name: "",
+    number: "",
+  })
+
+    const handleInput = (evt) => {
+      const { name, value } = evt.target;
+        setState((prev) => ({...prev, [name]: value }));
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        onSubmit(state);
+        reset()
+    }
+      const reset = () => {
+        setState('')
+      }
+
+        return (
+          <>
+          <h1 className={css.phonebookTitle}>Phonebook</h1>
+          <div className={css.borderContainer}>
+            <form className = {css.contactsForm} onSubmit={handleSubmit}>
+            <label htmlFor={nanoid()} className = {css.label}>
+            Name
+            <input id={nanoid()} className = {css.input}
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  value={state.name}
+  onChange={handleInput}
+  required
+/>
+</label>
+<label htmlFor= {nanoid()} className = {css.label}>Number
+<input id={nanoid()} className = {css.input}
+  type="tel"
+  name="number"
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+  value={state.number}
+  onChange={handleInput}
+  required
+/>
+</label>
+<button className = {css.addButton} type='submit'>Add contact</button>
+</form>
+</div>
+</>
+        )
+      }
+
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
+export default ContactForm;
